51649175368fff02ca62a1c24b7ccbde5669614b
ajouatom <ajouatom@gmail.com>
2025-07-18 20:32:04 +0900

    for PC(webcam)

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -79,7 +79,7 @@ procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
   NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
-  NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
+  NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad, enabled=not PC),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
   PythonProcess("logmessaged", "system.logmessaged", always_run),
 
