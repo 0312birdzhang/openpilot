b892e84681034bf676e1dd20c244ccdd2694a73b
BirdZhang <0312birdzhang@gmail.com>
2025-07-31 02:54:11 +0000

    revert to use normal opencv

--- tools/webcam/camera.py
+++ tools/webcam/camera.py
@@ -1,6 +1,5 @@
 import av
 import cv2 as cv
-import numpy as np
 
 class Camera:
   def __init__(self, cam_type_state, stream_type, camera_id):
@@ -14,34 +13,35 @@ class Camera:
 
     print(f"Opening {cam_type_state} at {camera_id}")
 
-    #self.cap = cv.VideoCapture(camera_id)
-    self.cap = cv.VideoCapture(f"gst-launch-1.0 v4l2src device={camera_id} io-mode=2 ! image/jpeg, width=1280, height=720, framerate=30/1, format=MJPG ! jpegdec ! videoconvert ! appsink", cv.CAP_GSTREAMER)
+    self.cap = cv.VideoCapture(camera_id)
 
-    #self.cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280.0)
-    #self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720.0)
-    #self.cap.set(cv.CAP_PROP_FPS, 25)
+    self.cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280.0)
+    self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720.0)
+    self.cap.set(cv.CAP_PROP_FPS, 25.0)
+    self.cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*'MJPG'))
 
     self.W = self.cap.get(cv.CAP_PROP_FRAME_WIDTH)
     self.H = self.cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+    self.fps = self.cap.get(cv.CAP_PROP_FPS)
+
+    self.cap.read()
+
+    print(f"W: {self.W} H: {self.H} fps: {self.fps} backend: {self.cap.getBackendName()}")
 
   @classmethod
   def bgr2nv12(self, bgr):
     frame = av.VideoFrame.from_ndarray(bgr, format='bgr24')
     return frame.reformat(format='nv12').to_ndarray()
 
-  def cv_bgr2nv12(self, bgr):
-    yuv = cv.cvtColor(bgr, cv.COLOR_BGR2YUV_I420)
-    uv_row_cnt = yuv.shape[0] // 3
-    uv_plane = np.transpose(yuv[uv_row_cnt * 2:].reshape(2, -1), [1, 0])
-    yuv[uv_row_cnt * 2:] = uv_plane.reshape(uv_row_cnt, -1)
-    return yuv.tobytes()
-
   def read_frames(self):
-    while True:
-      ret, frame = self.cap.read()
-      if not ret:
-        break
-      #yuv = Camera.bgr2nv12(frame)
-      yuv = self.cv_bgr2nv12(frame)
-      yield yuv
+    try:
+      while True:
+        sts , frame = self.cap.read()
+        if not sts:
+          print ("cv no sts")
+          break
+        yuv = Camera.bgr2nv12(frame)
+        yield yuv.data.tobytes()
+    except cv.error as error:
+      print(f"cv error: {error}")
     self.cap.release()
